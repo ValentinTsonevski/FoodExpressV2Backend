4ee8dfb180c21b8a09f84e5e0cd6a8e8b6dfd14e
ValentinTsonevski <tsonevski99@gmail.com>
2024-08-23 17:32:00 +0300

    project setup

---
+++ src/main/java/project/foodexpressv2/FoodExpressV2Application.java
@@ -0,0 +1,13 @@
+package project.foodexpressv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodExpressV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodExpressV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/project/foodexpressv2/FoodExpressV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.foodexpressv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodExpressV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
